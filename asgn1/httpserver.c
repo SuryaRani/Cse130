92b8d4bbac46ef4231c139abbf3cf1c0e9ffb6f5
Surya Rani <srani@ucsc.edu>
2020-04-30 01:31:31 -0700

    Close to finishing get just need to send the data i think and then head is easy and then put is last thing to do

--- asgn1/httpserver.c
+++ asgn1/httpserver.c
@@ -8,22 +8,91 @@
 #include <string.h> // memset
 #include <stdlib.h> // atoi
 #include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 4097
 
-void put(uint8_t *buffer, ssize_t length, int clientSock)
+void put(uint8_t *buffer, int length, int clientSock, char *file)
 {
     printf("GOT INTO PUt\n");
+    printf("This is the file: %s", file);
+    /*uint8_t readFile[length];
+    if (buffer.length < 4097)
+    {
+        write
+    }
+    size_t rd = read(op, readFile, length);
+    if (rd != 0)
+    {
+        if (rd == 10)
+        {
+            //need to figure out how to not print out again with the last buffer space
+            int w = write(STDOUT_FILENO, rdBuf, 10);
+            size_t keepRead = read(op, rdBuf, 10);
+            while (keepRead == 10 && w == 10)
+            {
+                w = write(STDOUT_FILENO, rdBuf, 10);
+                keepRead = read(op, rdBuf, 10);
+            }
+            if (keepRead < 10 && keepRead > 0)
+            {
+                w = write(STDOUT_FILENO, rdBuf, keepRead);
+            }
+            //printf("\n");
+        }
+        else
+        {
+            int w = write(STDOUT_FILENO, rdBuf, rd);
+            if (w < 0)
+            {
+            }
+        }
+    }*/
     dprintf(clientSock, "HTTP/1.1 201 CREATED\r\nContent-Length: 0\r\n\r\n");
 }
 
-void get(int clientSock)
+void get(int clientSock, char *file)
 {
-    printf("GOT INTO GET\n");
-    dprintf(clientSock, "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\n");
+    printf("This is the file: %s", file);
+    int op = open(file, O_RDONLY);
+    uint8_t rd[1000];
+    if (op >= 0)
+    {
+        printf("GOT INTO GET\n");
+        size_t reading = read(op, rd, 1000);
+        if (reading != 0)
+        {
+            if (reading == 1000)
+            {
+            }
+            else
+            {
+                size_t sending = send(clientSock, rd, reading, 0);
+            }
+        }
+
+        dprintf(clientSock, "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\n\r\n", reading);
+    }
+    else
+    {
+        int err = errno;
+        if (err == ENOENT)
+        {
+            dprintf(clientSock, "HTTP/1.1 404 File Not Found\r\n");
+        }
+        else if (err == EACCES)
+        {
+            dprintf(clientSock, "HTTP/1.1 403 File Forbidden\r\n");
+        }
+        else
+        {
+            dprintf(clientSock, "HTTP/1.1 500 Internal Server Error\r\n");
+        }
+    }
 }
 
-void head(int clientSock)
+void head(int clientSock, char *file)
 {
     printf("GOT INTO HEAD\n");
     dprintf(clientSock, "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\n");
@@ -110,16 +179,25 @@ int main(int argc, char **argv)
         //parsing algorithm
         token = strtok(buff, "\r\n");
         sscanf(token, "%s %s ", func, fileName);
+        //checks if first character is a / or not
+        if (fileName[0] == '/')
+        {
+            memmove(fileName, fileName + 1, strlen(fileName));
+        }
+        printf("This is filenme: %s\n", fileName);
+        printf("also this is fiirst char: %c", fileName[0]);
         //check if filename violates any of the rules
         // first check the size make sure it is less than or equal to 27 characters
-        if (strlen(fileName) > 28)
+
+        if (strlen(fileName) > 27)
         {
             dprintf(client_sockd, "HTTP/1.1 400 BAD REQUEST\r\n\r\n");
             //printf("in filename siz\n");
             close(client_sockd);
         }
+
         //check if filename is alphanumeric or contains -, _
-        for (int i = 1; i < strlen(fileName); i++)
+        for (int i = 0; i < strlen(fileName); i++)
         {
             if (!((isalpha(fileName[i])) != 0 || (isdigit(fileName[i]) != 0) || fileName[i] == '-' || fileName[i] == '_'))
             {
@@ -170,28 +248,21 @@ int main(int argc, char **argv)
 
         if (strcmp(func, "HEAD") == 0)
         {
-            head(client_sockd);
+            head(client_sockd, fileName);
             close(client_sockd);
             //dprintf(client_sockd, "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\n");
         }
         else if (strcmp(func, "PUT") == 0)
         {
-            printf("THIS IS THE PRLENGTH: %s", preLen);
-            printf("THE CONTENT LENGTH: %d", conLen);
-            if (strcmp("Content-Length: ", preLen) != 0)
-            {
-                dprintf(client_sockd, "HTTP/1.1 400 BAD REQUEST\r\n\r\n");
-                close(client_sockd);
-            }
-            else
-            {
-                put(buff, conLen, client_sockd);
-                close(client_sockd);
-            }
+
+            //there might be an error here since we are not checking if the words before the content length is exactly content length
+            // i think well be fine because of the sscanf which has that string in the formating
+            put(buff, conLen, client_sockd, fileName);
+            close(client_sockd);
         }
         else if (strcmp(func, "GET") == 0)
         {
-            get(client_sockd);
+            get(client_sockd, fileName);
             close(client_sockd);
             //dprintf(client_sockd, "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\n");
         }
